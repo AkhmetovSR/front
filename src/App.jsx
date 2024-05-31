71137f097dd8e58749ab7fa91f9d3adacc4ee69b
AkhmetoSR <ahmetov26@gmail.com>
2024-05-31 12:51:16 +0500

    22

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import s from './App.module.css';
+
+const tg = window.Telegram.WebApp;
+
+
+
+function App() {
+    return (
+        <div className={s.App}>
+            <div className={s.btn}>asd</div>
+            <div className={s.btn}>asd</div>
+            <div className={s.btn}></div>
+            <div className={s.btn}></div>
+            <div className={s.btn}></div>
+        </div>
+    );
+}
+
+export default App;
